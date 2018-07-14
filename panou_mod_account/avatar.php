065cd6c88b2a8bcd7ae37151092962f08e2a2b2b
INFERNUS <vly.2814@gmail.com>
2018-07-14 23:30:40 +0300

    fix exploits shimbare poza profil

--- panou_mod_account/avatar.php
+++ panou_mod_account/avatar.php
@@ -2,8 +2,37 @@
 session_start();
 
      if(isset($_POST['submit']))
-        {
-         $file = "images/".$_SESSION["picture"];
+   
+   { 
+       
+       	
+			
+     
+       
+     $types = array('image/jpeg', 'image/gif', 'image/jpg' , 'image/png');
+   
+   
+    if (in_array($_FILES['file']['type'], $types)) 
+    {
+        
+        
+                       
+    	$connection = new mysqli('mysql.hostinger.com', 'u784726611_teze', 'b567c63b567c63', 'u784726611_teze');
+        $firstName = $connection->real_escape_string($_POST["firstName"]);  		
+		$email = $connection->real_escape_string($_POST["email"]);  
+		$password = sha1($connection->real_escape_string($_POST["password"])); 
+	
+		$data = $connection->query("INSERT INTO users (username, email, password) VALUES ('$firstName', '$email', '$password')");
+		
+    	if ($data === false)
+        	echo "<div class='alert'>
+             <span class=\"closebtn\" onclick=\"this.parentElement.style.display='none';\">×</span>
+               Există deja un cont creat cu această adresă de e-mail.
+              </div>";
+       
+       
+
+  $file = "images/".$_SESSION["picture"];
     unlink($file);
            if(file_exists('images/default.jpg'))
                move_uploaded_file($_FILES['file']['tmp_name'],"images/".$_FILES['file']['name']);
@@ -31,5 +60,13 @@ session_start();
 
   }
           }
-?>
+       
+ 
+    }	
+        
+ 
+   
+?> 
+            
+
     
